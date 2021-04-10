cede0c00415f787ff87825a2b322f2a10fc4b7ff
Vedant <iamsinghved@gmail.com>
2021-04-10 19:15:45 +0530

    python: Trapping Rain Water

--- Python/trapping_rain_water.py
+++ Python/trapping_rain_water.py
@@ -1,19 +1,35 @@
-# GfG solution
-class Solution:
-	def trap(self,height):
-		res = 0
-		n = len(height)
-		for i in range(1, n - 1): 
-			left = height[i]
-			for j in range(i):
-				left = max(left, height[j])
-			right = height[i]
-			for j in range(i + 1 , n):
-				right = max(right, height[j])
-			res = res + (min(left, right) - height[i])
-		return res
-'''
+#taken from discussion tab of leetcode
+class Solution(object):
+    def trap(self, height):
+	
+		# edge cases
+        if not height or len(height) < 3:
+            return 0
+        
+        n = len(height)
+        
+        l_max = [None] * n
+        l_max[0] = height[0] # setting the last element
+        
+        r_max = [None] * n
+        r_max[n-1] = height[n-1] # setting the first element
 
+        i = 1 # i starts from height[1] instead of height[0]
+        j = n-2 # j starts from height[n-2] instead of height[n-1]
+        
+        while i < n and j >= 0: # filling leftMax and rightMax arrays in one single loop
+            l_max[i] = max(l_max[i-1],height[i])
+            r_max[j] = max(r_max[j+1], height[j])
+            i+=1
+            j-=1
+        
+		# getting the final ans ie.  totalsum
+        total_sum = 0
+        for i in range(1, n-1):
+            total_sum += min(r_max[i], l_max[i]) - height[i]
+        
+        return total_sum
+'''
 My Solution
 
 class Solution:
